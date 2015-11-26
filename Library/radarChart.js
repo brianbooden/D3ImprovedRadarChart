02df9807c3820f88c93a070b99c2374526a67f31
VizMatt <matthieu.burel@gmail.com>
2015-11-26 22:56:06 +0100

    Number format

--- Library/radarChart.js
+++ Library/radarChart.js
@@ -34,22 +34,7 @@ function displayRADAR(id, options, $element, layout, data, self) {
 		if('undefined' !== typeof options[i]){ cfg[i] = options[i]; }
 	  }//for i
 	}//if
-	
-	//Calculate the average value for each area
-//	data.forEach(function(d){
-//		d["value" + "Average"] = d3.mean(d,function(e) { return e.value}); 
-//	})
-	
-	//Sort the data for the areas from largest to smallest
-	//by average value as an approximation of actual blob area
-	//so that that the smallest area is drawn last
-	//and therefore appears on top
-//	data = data.sort(function(a, b){
-//		var a = a["value" + "Average"],
-//				b = b["value" + "Average"];
-//		return b - a;
-//	})
-	
+		
 	//If the supplied maxValue is smaller than the actual one, replace by the max in the data
 	var maxValue = Math.max(cfg.maxValue, d3.max(data, function(i){return d3.max(i.map(function(o){return o.value;}))}));
 
@@ -60,7 +45,6 @@ function displayRADAR(id, options, $element, layout, data, self) {
 		radius = Math.min(
 			(graphW/2) - cfg.margin.left - cfg.margin.right,
 			(graphH/2) - cfg.margin.top - cfg.margin.bottom), 				//Radius of the outermost circle
-		Format = d3.format('%'),			 								//Percentage formatting
 		angleSlice = Math.PI * 2 / total;									//The width in radians of each "slice"
 	
 	//Scale for the radius
@@ -74,12 +58,6 @@ function displayRADAR(id, options, $element, layout, data, self) {
 
 	//Remove whatever chart with the same id/class was present before
 	d3.select(id).select("svg").remove();
-	
-	//Initiate the radar chart SVG
-	//var svg = d3.select(id).append("svg")
-	//		.attr("width",  cfg.size.width + cfg.margin.left + cfg.margin.right)
-	//		.attr("height", cfg.size.height + cfg.margin.top + cfg.margin.bottom)
-	//		.attr("class", "radar"+id);
 			
 	// Chart object width  
    var width = cfg.size.width - cfg.margin.left - cfg.margin.right;  
@@ -158,7 +136,7 @@ function displayRADAR(id, options, $element, layout, data, self) {
 	   .attr("dy", "0.4em")
 	   .style("font-size", "10px")
 	   .attr("fill", "#737373")
-	   .text(function(d,i) { return Format(maxValue * d/cfg.levels); });
+	   .text(function(d,i) { return format(options.numberFormat[0], (maxValue * d/cfg.levels)*options.numberFormat[1]) + options.numberFormat[2]; });
 
 	/////////////////////////////////////////////////////////
 	//////////////////// Draw the axes //////////////////////
@@ -297,7 +275,7 @@ function displayRADAR(id, options, $element, layout, data, self) {
 			tooltip
 				.attr('x', newX)
 				.attr('y', newY)
-				.text(d.radar_area + " : " + Format(d.value))
+				.text(d.radar_area + " : " + format(options.numberFormat[0], d.value*options.numberFormat[1]) + options.numberFormat[2])
 				.transition().duration(200)
 				.style('opacity', 1);
 		})
@@ -405,5 +383,69 @@ function displayRADAR(id, options, $element, layout, data, self) {
 		svg.select(".legendOrdinal")
 		  .call(legendOrdinal);
 	}
+	
+	function format( m, v){ 
+		if (!m || isNaN(+v)) {
+			return v; //return as it is.
+		}
+		//convert any string to number according to formation sign.
+		var v = m.charAt(0) == '-'? -v: +v;
+		var isNegative = v<0? v= -v: 0; //process only abs(), and turn on flag.
+		
+		//search for separator for grp & decimal, anything not digit, not +/- sign, not #.
+		var result = m.match(/[^\d\-\+#]/g);
+		var Decimal = (result && result[result.length-1]) || '.'; //treat the right most symbol as decimal 
+		var Group = (result && result[1] && result[0]) || ',';  //treat the left most symbol as group separator
+		
+		//split the decimal for the format string if any.
+		var m = m.split( Decimal);
+		//Fix the decimal first, toFixed will auto fill trailing zero.
+		v = v.toFixed( m[1] && m[1].length);
+		v = +(v) + ''; //convert number to string to trim off *all* trailing decimal zero(es)
+
+		//fill back any trailing zero according to format
+		var pos_trail_zero = m[1] && m[1].lastIndexOf('0'); //look for last zero in format
+		var part = v.split('.');
+		//integer will get !part[1]
+		if (!part[1] || part[1] && part[1].length <= pos_trail_zero) {
+			v = (+v).toFixed( pos_trail_zero+1);
+		}
+		var szSep = m[0].split( Group); //look for separator
+		m[0] = szSep.join(''); //join back without separator for counting the pos of any leading 0.
+
+		var pos_lead_zero = m[0] && m[0].indexOf('0');
+		if (pos_lead_zero > -1 ) {
+			while (part[0].length < (m[0].length - pos_lead_zero)) {
+				part[0] = '0' + part[0];
+			}
+		}
+		else if (+part[0] == 0){
+			part[0] = '';
+		}
+		
+		v = v.split('.');
+		v[0] = part[0];
+		
+		//process the first group separator from decimal (.) only, the rest ignore.
+		//get the length of the last slice of split result.
+		var pos_separator = ( szSep[1] && szSep[ szSep.length-1].length);
+		if (pos_separator) {
+			var integer = v[0];
+			var str = '';
+			var offset = integer.length % pos_separator;
+			for (var i=0, l=integer.length; i<l; i++) { 
+				
+				str += integer.charAt(i); //ie6 only support charAt for sz.
+				//-pos_separator so that won't trail separator on full length
+				if (!((i-offset+1)%pos_separator) && i<l-pos_separator ) {
+					str += Group;
+				}
+			}
+			v[0] = str;
+		}
+
+		v[1] = (m[1] && v[1])? Decimal+v[1] : "";
+		return (isNegative?'-':'') + v[0] + v[1]; //put back any negation and combine integer and fraction.
+	};
 	
 }//RadarChart
