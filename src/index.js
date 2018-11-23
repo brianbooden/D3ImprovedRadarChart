ec7d9a516f7cb6be59c28527f9ce73f5799b9a35
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2018-11-23 11:53:09 +0100

    settings panel object set to match Qlik standard object

--- src/index.js
+++ src/index.js
@@ -22,19 +22,30 @@ export default {
       }]
     }
   },
+  data: {
+    dimensions: {
+      min: 2,
+      max: 2
+    },
+    measures: {
+      min: 1,
+      max: 1
+    }
+  },
   definition: {
     type: "items",
     component: "accordion",
     items: {
-      dimensions: {
-        uses: "dimensions",
-        min: 2,
-        max: 2
-      },
-      measures: {
-        uses: "measures",
-        min: 1,
-        max: 1
+      data: {
+        uses: "data",
+        items: {
+          dimensions:{
+            disabledRef: ""
+          },
+          measures: {
+            disabledRef: ""
+          }
+        }
       },
       sorting: {
         uses: "sorting"
@@ -42,88 +53,94 @@ export default {
       settings : {
         uses : "settings",
         items : {
-          Line:{
-            ref: "strokeStyle",
-            component: "dropdown",
-            type: "boolean",
-            label: "Stroke type",
-            defaultValue:  true,
-            options: [{
-              value: true,
-              label: "Smooth"
-            }, {
-              value: false,
-              label: "Straight"
-            }],
-            show: true
-          },
-          Legend:{
-            ref: "showLegend",
-            component: "switch",
-            type: "boolean",
-            translation: "Legend",
-            defaultValue: true,
-            trueOption: {
-              value: true,
-              translation: "properties.on"
-            },
-            falseOption: {
-              value: false,
-              translation: "properties.off"
-            },
-            show: true
-          },
-          colors: {
-
+          design: {
+            label: "Design",
             type: "items",
-            label: "Colors",
-            items : {
-              ColorSchema: {
-                ref: "ColorSchema",
-                type: "string",
+            items: {
+              Line:{
+                ref: "strokeStyle",
                 component: "dropdown",
-                label: "Color",
-                show: true,
-                options: [
-                  {
-                    value: "#fee391, #fec44f, #fe9929, #ec7014, #cc4c02, #993404, #662506",
-                    label: "Sequential"
-                  }, {
-                    value: "#662506, #993404, #cc4c02, #ec7014, #fe9929, #fec44f, #fee391",
-                    label: "Sequential (Reverse)"
-                  }, {
-                    value: "#d73027, #f46d43, #fee090, #abd9e9, #74add1, #4575b4",
-                    label: "Diverging RdYlBu"
-                  }, {
-                    value: "#4575b4, #74add1, #abd9e9, #fee090, #f46d43, #d73027",
-                    label: "Diverging BuYlRd (Reverse)"
-                  }, {
-                    value: "#deebf7, #c6dbef, #9ecae1, #6baed6, #4292c6, #2171b5, #08519c, #08306b",
-                    label: "Blues"
-                  }, {
-                    value: "#fee0d2, #fcbba1, #fc9272, #fb6a4a, #ef3b2c, #cb181d, #a50f15, #67000d",
-                    label: "Reds"
-                  }, {
-                    value: "#edf8b1, #c7e9b4, #7fcdbb, #41b6c4, #1d91c0, #225ea8, #253494, #081d58",
-                    label: "YlGnBu"
-                  }, {
-                    value: "#332288, #6699CC, #88CCEE, #44AA99, #117733, #999933, #DDCC77, #661100, #CC6677, #AA4466, #882255, #AA4499",
-                    label: "12 colors"
-                  }, {
-                    value: "#AA4499, #882255, #AA4466, #CC6677, #661100, #DDCC77, #999933, #117733, #44AA99, #88CCEE, #6699CC, #332288",
-                    label: "12 colors (Reverse)"
-                  }, {
-                    value: "#1ABC9C, #7F8C8D, #2ECC71, #BDC3C7, #3498DB, #C0392B, #9B59B6, #D35400, #34495E, #F39C12, #16A085, #95A5A6",
-                    label: "Blue purple colors"
-                  }, {
-                    value: "#BE4FC0, #4477AA",
-                    label: "2 colors"
-                  }, {
-                    value: "#4477AA, #BE4FC0",
-                    label: "2 colors (Reverse)"
+                type: "boolean",
+                label: "Stroke type",
+                defaultValue:  true,
+                options: [{
+                  value: true,
+                  label: "Smooth"
+                }, {
+                  value: false,
+                  label: "Straight"
+                }],
+                show: true
+              },
+              Legend:{
+                ref: "showLegend",
+                component: "switch",
+                type: "boolean",
+                translation: "Legend",
+                defaultValue: true,
+                trueOption: {
+                  value: true,
+                  translation: "properties.on"
+                },
+                falseOption: {
+                  value: false,
+                  translation: "properties.off"
+                },
+                show: true
+              },
+              colors: {
+
+                type: "items",
+                label: "Colors",
+                items : {
+                  ColorSchema: {
+                    ref: "ColorSchema",
+                    type: "string",
+                    component: "dropdown",
+                    label: "Color",
+                    show: true,
+                    options: [
+                      {
+                        value: "#fee391, #fec44f, #fe9929, #ec7014, #cc4c02, #993404, #662506",
+                        label: "Sequential"
+                      }, {
+                        value: "#662506, #993404, #cc4c02, #ec7014, #fe9929, #fec44f, #fee391",
+                        label: "Sequential (Reverse)"
+                      }, {
+                        value: "#d73027, #f46d43, #fee090, #abd9e9, #74add1, #4575b4",
+                        label: "Diverging RdYlBu"
+                      }, {
+                        value: "#4575b4, #74add1, #abd9e9, #fee090, #f46d43, #d73027",
+                        label: "Diverging BuYlRd (Reverse)"
+                      }, {
+                        value: "#deebf7, #c6dbef, #9ecae1, #6baed6, #4292c6, #2171b5, #08519c, #08306b",
+                        label: "Blues"
+                      }, {
+                        value: "#fee0d2, #fcbba1, #fc9272, #fb6a4a, #ef3b2c, #cb181d, #a50f15, #67000d",
+                        label: "Reds"
+                      }, {
+                        value: "#edf8b1, #c7e9b4, #7fcdbb, #41b6c4, #1d91c0, #225ea8, #253494, #081d58",
+                        label: "YlGnBu"
+                      }, {
+                        value: "#332288, #6699CC, #88CCEE, #44AA99, #117733, #999933, #DDCC77, #661100, #CC6677, #AA4466, #882255, #AA4499",
+                        label: "12 colors"
+                      }, {
+                        value: "#AA4499, #882255, #AA4466, #CC6677, #661100, #DDCC77, #999933, #117733, #44AA99, #88CCEE, #6699CC, #332288",
+                        label: "12 colors (Reverse)"
+                      }, {
+                        value: "#1ABC9C, #7F8C8D, #2ECC71, #BDC3C7, #3498DB, #C0392B, #9B59B6, #D35400, #34495E, #F39C12, #16A085, #95A5A6",
+                        label: "Blue purple colors"
+                      }, {
+                        value: "#BE4FC0, #4477AA",
+                        label: "2 colors"
+                      }, {
+                        value: "#4477AA, #BE4FC0",
+                        label: "2 colors (Reverse)"
+                      }
+                    ],
+                    defaultValue: "#332288, #6699CC, #88CCEE, #44AA99, #117733, #999933, #DDCC77, #661100, #CC6677, #AA4466, #882255, #AA4499"
                   }
-                ],
-                defaultValue: "#332288, #6699CC, #88CCEE, #44AA99, #117733, #999933, #DDCC77, #661100, #CC6677, #AA4466, #882255, #AA4499"
+                }
               }
             }
           }
